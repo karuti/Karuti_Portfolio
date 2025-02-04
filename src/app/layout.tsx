b0b380c4321602ce8bfb280936a0509cfccd2c4c
Karuti Joy <karutijoy@Karutis-MacBook-Pro.local>
2025-02-04 22:12:22 +0300

    Added a font pre-loader

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import GlobalStyle from "../app/styles/GlobalStyles.js"
 import { Analytics } from "@vercel/analytics/react"
+import Head from 'next/head';
 
 export const metadata: Metadata = {
   title: "Karuti",
@@ -9,6 +10,16 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <Head>
+        {/* Preload your custom font */}
+        <link
+          rel="preload"
+          href="/font/GeneralSans-Regular.woff2"
+          as="font"
+          type="font/woff2"
+          crossOrigin="anonymous"
+        />
+      </Head>
       <body>
         <GlobalStyle />
         {children}
